33e6c2cb97208963ae1f20560616c3dcb75f484f
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2021-06-27 00:37:48 +0300

    solution task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
-const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
-];
+const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
+
+const ingredientsEl = document.getElementById('ingredients');
+
+// =========== создает отдельный li =========== //
+
+const makeRecipe = ingredient => {
+  const liItemEl = document.createElement('li');
+  liItemEl.classList.add('ingredients__item');
+  liItemEl.textContent = ingredient;
+  return liItemEl;
+};
+
+// =========== вставит все li за одну операцию в список ul =========== //
+
+ingredients.forEach(ingredient => {
+  return ingredientsEl.append(makeRecipe(ingredient));
+});
