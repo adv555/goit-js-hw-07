56704839c7bb061db2d47efedd753bb39dd52d1f
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2021-06-27 00:37:55 +0300

    solution task-3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,23 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// ==== создание разметки тегов img вложенных в li ==== //
+
+const addGalleryCardMarkup = ({ url, alt }) =>
+  `<li class="card" ><img class="card__image" src= ${url} alt='${alt}' width = 100%, height = auto/></li>`;
+
+// ==== Элементы галереи добавляются в DOM ==== //
+
+const galleryEl = document.getElementById('gallery');
+const addGalleryCard = images.map(image => {
+  return galleryEl.insertAdjacentHTML('afterbegin', addGalleryCardMarkup(image));
+});
+
+// ==== минимальное оформление галереи флексбоксами ==== //
+
+galleryEl.style.display = 'flex';
+galleryEl.style.flexWrap = 'wrap';
+galleryEl.style.alignContent = 'center';
+galleryEl.style.listStyle = 'none';
+galleryEl.style.padding = '15px';
