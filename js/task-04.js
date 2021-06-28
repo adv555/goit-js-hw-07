e8dbf2ce287ef24916ebf23c944f35d51424eb40
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2021-06-28 23:27:52 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,17 +1,19 @@
+const refs = {
+  targetBtnIncr: document.querySelector('[data-action="increment"]'),
+  targetBtnDecr: document.querySelector('[data-action="decrement"]'),
+};
+
 let counterValue = 0;
 
 const increment = () => {
   counterValue += 1;
-  return (document.getElementById('value').innerHTML = counterValue);
+  return (document.getElementById('value').textContent = counterValue);
 };
 
 const decrement = () => {
   counterValue -= 1;
-  return (document.getElementById('value').innerHTML = counterValue);
+  return (document.getElementById('value').textContent = counterValue);
 };
 
-const targetBtnIncr = document.querySelector('[data-action="increment"]');
-const targetBtnDecr = document.querySelector('[data-action="decrement"]');
-
-targetBtnIncr.addEventListener('click', increment);
-targetBtnDecr.addEventListener('click', decrement);
+refs.targetBtnIncr.addEventListener('click', increment);
+refs.targetBtnDecr.addEventListener('click', decrement);
